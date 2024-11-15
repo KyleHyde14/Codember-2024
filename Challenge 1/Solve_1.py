427b9e550549ac1cd68fa01d254937bf6ebe9722
KyleHyde14 <jesushdezjob@gmail.com>
2024-11-15 11:10:13 +0100

    first commit

---
+++ Challenge 1/Solve_1.py
@@ -0,0 +1,12 @@
+nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+original = '528934712834'
+
+password = ''
+
+moves = 'URDURUDRUDLLLLUUDDUDUDUDLLRRRR'
+
+for i in original:
+    
+
+    
