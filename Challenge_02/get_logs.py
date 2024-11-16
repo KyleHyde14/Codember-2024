76c4bbbf00d6b4caff28f6b68675d2564734f7ff
KyleHyde14 <jesushdezjob@gmail.com>
2024-11-16 01:00:32 +0100

    finished second challenge

--- Challenge_02/get_logs.py
+++ Challenge_02/get_logs.py
@@ -3,3 +3,5 @@ logs_list = []
 with open('Challenge_02/logs.txt') as topo_file:
     for line in topo_file:
         logs_list.append(line.strip())
+
+# print(logs_list) to copy and paste into the main script
